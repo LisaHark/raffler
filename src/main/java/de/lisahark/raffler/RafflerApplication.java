485f1b9960c7c82652fbc18af6c7754ba3a35cfa
LHark <lisahark@hotmail.de>
2021-08-16 18:55:13 +0200

    initial

---
+++ src/main/java/de/lisahark/raffler/RafflerApplication.java
@@ -0,0 +1,13 @@
+package de.lisahark.raffler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RafflerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RafflerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lisahark/raffler/RafflerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lisahark.raffler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RafflerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
